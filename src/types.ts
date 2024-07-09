9db2490d03a947cb4683941d46bc7830f04096a2
Jackson Geer <jacksongeer@gmail.com>
2024-07-09 09:33:13 -0400

    added services and types config

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@ export interface Options {
         headers?: HttpHeaders | {
             [header: string]: string | string[];
         };
-        observe: 'body';
+        observe?: 'body';
         context?: HttpContext;
         params?: HttpParams | {
             [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>;
@@ -17,4 +17,26 @@ export interface Options {
             includeHeaders?: string[];
         } | boolean;
     
+}
+
+export interface Products {
+    items: Product[];
+    total: number;
+    page: number;
+    perPage: number;
+    totalPages: number;
+
+}
+
+export interface Product {
+    price: string;
+    name: string;
+    image: string;
+    rating: number;
+}
+
+export interface PaginationParams {
+[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>;
+ page: number;
+ perPage: number;
 }
