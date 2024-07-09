6d570fd295164e00246641bd54fef08ff94e16a5
Jackson Geer <jacksongeer@gmail.com>
2024-07-08 22:55:43 -0400

    setting up interface

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+import { HttpContext, HttpHeaders, HttpParams } from "@angular/common/http";
+
+export interface Options {
+    
+        headers?: HttpHeaders | {
+            [header: string]: string | string[];
+        };
+        observe: 'events';
+        context?: HttpContext;
+        params?: HttpParams | {
+            [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>;
+        };
+        reportProgress?: boolean;
+        responseType?: 'json';
+        withCredentials?: boolean;
+        transferCache?: {
+            includeHeaders?: string[];
+        } | boolean;
+    
+}
